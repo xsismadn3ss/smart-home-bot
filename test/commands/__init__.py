395dc3bfc38d8c595f771634b52f18561ba89358
Abraham <132122513+xsismadn3ss@users.noreply.github.com>
2024-08-28 22:43:20 +0000

    auto import file

---
+++ test/commands/__init__.py
@@ -0,0 +1,9 @@
+print("loading commands")
+
+from .data import bot, numbers
+from .help import help
+from .hour import send_time
+from .load_save_info import *
+from .repeat import repeat
+
+print("Sucess 🎉")
