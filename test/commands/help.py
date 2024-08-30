090caf735ae97de9f4c492af790758c4771da96e
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-30 01:48:39 -0600

    delete help file

--- test/commands/help.py
+++
@@ -1,8 +0,0 @@
-from .data import bot
-@bot.message_handler(commands=["help"])
-def help(message):
-    print("sending description...")
-    bot.reply_to(
-        message,
-        "Este es un bot de prueba hecho en telegram\n\nLista de comandos:\n/hour - obtener hora actual\n/save_data - guardar datos en la base de datos\n/get_numbers - obtener numeros en la base de datos",
-    )
