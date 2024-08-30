5f1bfc7fccf1b2dfdc2937b69fc6474b43601c82
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-30 10:27:22 -0600

    actualizar descripción

--- test/commands/description.py
+++ test/commands/description.py
@@ -2,9 +2,9 @@ from .data import bot
 
 
 @bot.message_handler(commands=["help"])
-def help(message):
+async def help(message):
     print("sending description...")
-    bot.reply_to(
+    await bot.reply_to(
         message,
         "Este es un bot de prueba hecho en telegram\n\nLista de comandos:\n/hour - obtener hora actual\n/save_data - guardar datos en la base de datos\n/get_numbers - obtener numeros en la base de datos",
     )
