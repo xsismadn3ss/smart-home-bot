45c238253cfaa62ee21b6c65acccb19f283ddd37
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-30 01:50:22 -0600

    make start command into async

--- test/commands/start.py
+++ test/commands/start.py
@@ -1,6 +1,6 @@
 from .data import bot
 
 @bot.message_handler(commands=["start"])
-def welcome(message):
+async def welcome(message):
     print("greeting...")
-    bot.reply_to(message, "Hola, bienvenido!!!")
+    await bot.reply_to(message, "Hola, bienvenido!!!")
