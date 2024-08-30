dd336cf610757c3d1a5920c9b190de0f123be916
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-30 01:49:46 -0600

    make async functions in information handler

--- test/commands/load_save_info.py
+++ test/commands/load_save_info.py
@@ -1,31 +1,33 @@
 from .data import bot
 from .data import numbers
+from telebot.async_telebot import AsyncTeleBot
 
 @bot.message_handler(commands=["save_data"])
-def save_number(message):
+async def save_number(message):
     print("ask info")
     chat_id = message.chat.id
-    bot.send_message(chat_id, "Ingresa un numero para guardar")
-    bot.register_next_step_handler(message, process_number)
+    numbers[chat_id] = 'waiting_for_number'
+    await bot.send_message(chat_id, "Ingresa un numero para guardar")
 
 
-def process_number(message):
+@bot.message_handler(func=lambda message: numbers.get(message.chat.id)== 'waiting_for_number')
+async def process_number(message):
     try:
         print("saving info...")
         numero = int(message.text)
-        numbers.append(numero)
-        bot.send_message(message.chat.id, f"Numero {numero} guardado exitosamente")
+        await bot.send_message(message.chat.id, f"Numero {numero} guardado exitosamente")
+        numbers[message.chat.id] = None
     except ValueError:
-        bot.send_message(message.chat.id, "Por favor envia un número valido")
+        await bot.send_message(message.chat.id, "Por favor envia un número valido")
 
 
 @bot.message_handler(commands=["get_numbers"])
-def load_numbers(message):
+async def load_numbers(message):
     try:
         print("loading info...")
         msg = ""
         for i in numbers:
             msg += f"{i}\n"
-        bot.reply_to(message, msg)
+        await bot.reply_to(message, msg)
     except Exception as e:
-        bot.send_message(message.chat.id, "Aún no hay información guardada")
+        await bot.send_message(message.chat.id, "Aún no hay información guardada")
