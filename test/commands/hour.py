967c703a361051ed8ad3b9ef13ba07311faee44a
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-30 01:49:18 -0600

    make send_time async

--- test/commands/hour.py
+++ test/commands/hour.py
@@ -1,8 +1,10 @@
 from .data import bot
+
+
 @bot.message_handler(commands=["hour"])
-def send_time(message):
+async def send_time(message):
     import datetime
 
     now = datetime.datetime.now()
     print("sending time")
-    bot.reply_to(message, f"Son las {now.hour}:{now.minute}")
+    await bot.reply_to(message, f"Son las {now.hour}:{now.minute}")
