f92ba49b10b485cbb20011546a1fa0da1d3b69e1
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-30 01:47:29 -0600

    edit __init__

--- test/commands/__init__.py
+++ test/commands/__init__.py
@@ -1,7 +1,7 @@
 print("loading commands")
 
 from .data import bot, numbers
-from .help import help
+from .description import help
 from .hour import send_time
 from .load_save_info import *
 from .repeat import repeat
