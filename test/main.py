aea0ed6fae16fc80d6084107d413516446851213
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-30 01:47:44 -0600

    add async functions

--- test/main.py
+++ test/main.py
@@ -1,6 +1,20 @@
+import asyncio
 from commands import *
 from commands.data import bot
 
-print("bot initialized 🤖")
+async def count():
+    i = 1
+    while True:
+        print(f"Lectura {i}")
+        i+=1
+        await asyncio.sleep(1)
 
-bot.polling()
+async def main():
+    count_task = asyncio.create_task(count())
+    asyncio.gather(count_task)
+
+    print("bot initialized 🤖")
+    await bot.polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
