30206b748fdbd75167cfe3c35566210a94274ca1
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-05 01:51:51 -0600

    update earnings.py

--- test/commands/earnings.py
+++ test/commands/earnings.py
@@ -1,19 +1,18 @@
 from .data import bot
 from .data import temp
-from telebot.async_telebot import AsyncTeleBot
 from .queries.earn_queries import get_all, insert
 
 @bot.message_handler(commands=["save_earn"])
-async def save_number(message):
+async def save_earning(message):
     print("ask info")
     chat_id = message.chat.id
-    temp[chat_id] = 'waiting_for_info'
+    temp[chat_id] = 'saving earning'
     await bot.reply_to(message, "Ingresa tu ganancia en el siguiente formato...")
     await bot.send_message(chat_id, "'cantidad' | ' razon'")
 
 
-@bot.message_handler(func=lambda message: temp.get(message.chat.id)== 'waiting_for_info')
-async def process_number(message):
+@bot.message_handler(func=lambda message: temp.get(message.chat.id)== 'saving earning')
+async def process_data(message):
     try:
         print("saving info...")
         success = await insert((message.text))
@@ -22,19 +21,19 @@ async def process_number(message):
         else:
             await bot.send_message(message.chat.id, "Error guardando los datos")
         temp[message.chat.id] = None
-    except ValueError:
-        await bot.reply_to(message, "Por favor envia un número valido")
+    except Exception as e:
+        await bot.reply_to(message, f"Ha sucedido un error {e}")
 
 
 @bot.message_handler(commands=["get_earnings"])
-async def load_numbers(message):
+async def load_earnings(message):
     try:
         print("loading info...")
         msg = ""
         data = await get_all()
         for d in data:
             msg += f"- {d[2]} | {d[3]}\n"
-        await bot.reply_to(message, f"Ganancias")
+        await bot.reply_to(message, "Ganancias: ")
         await bot.send_message(message.chat.id, msg)
     except Exception as e:
         print(e)
