bd27715063cf1bcaf3498d3657457568f8234295
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-05 01:54:21 -0600

    update main.py

--- test/main.py
+++ test/main.py
@@ -1,13 +1,23 @@
 import asyncio
 from commands import *
-from commands.data import bot
+from commands.data import bot, temp
+from commands.queries.users_queries import get_users
+import datetime
 
 async def count():
     i = 1
+    ids = await get_users()
     while True:
-        print(f"Lectura {i}")
-        i+=1
+        print(datetime.datetime.now())
+        i += 1
         await asyncio.sleep(1)
+        if i == 10:
+            print("event trigger")
+            for id in ids:
+                chat_id=id[1]
+                print("Sending message to ", chat_id)
+                await bot.send_message(chat_id, "Un evento ha ocurrido!!!")
+                
 
 async def main():
     count_task = asyncio.create_task(count())
