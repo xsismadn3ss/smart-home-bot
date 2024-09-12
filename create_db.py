2dcebd47969d5d9db376287f5c1418ff10a2b70d
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-11 18:36:39 -0600

    update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,15 +1,23 @@
 sqlcommands = [
-    """CREATE TABLE User (
+    # user
+    """CREATE TABLE User ( 
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );""",
+    # temperatur
     """CREATE TABLE Temperature (
     value REAL NOT NULL,
     date DATETIME NOT NULTT
 );""",
+    # humidity
     """CREATE TABLE Humidity (
     value REAL NOT NULL,
     date DATETIME NPT NULL
+);""",
+    """CREATE TABLE Graphics (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image_path TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );""",
 ]
