38ae76d1e29d668b8116356629932dea39275bc3
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-13 21:43:55 -0600

    edit test dht sensor

--- test_dht_sensor.py
+++ test_dht_sensor.py
@@ -1,11 +1,13 @@
 import time
-import Adafruit_DHT
+from raspberry import read_dht
+# import Adafruit_DHT
 
-sensor = Adafruit_DHT.DHT11
-pin = 4
+# sensor = Adafruit_DHT.DHT11
+# pin = 4
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    # humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    humidity, temperature = read_dht()
 
     if humidity is not None and temperature is not None:
         print(f'Humedad: {humidity:.1f}%\nTemperatura: {temperature:.1f}°C\n')
