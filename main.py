bbf0e61aeffbe8746822bf3b086ee717e8502e55
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-13 21:42:49 -0600

    add raspberry task

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import asyncio
 import datetime
 from src import *
-from src.bot_data import bot, temp
+from src.bot_data import bot
+
+async def raspberry():
+    while True:
+        await asyncio.sleep(2)
 
 
 async def main():
+    rasp_task = asyncio.create_task(raspberry())
+    asyncio.gather(rasp_task)
     await bot.polling()
 
 if __name__ == "__main__":
