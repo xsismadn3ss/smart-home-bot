7bafa7ba49fe1df8e0e6486acec13243609df89a
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-17 10:11:23 -0600

    update read status command

--- raspberry/read.py
+++ raspberry/read.py
@@ -3,3 +3,7 @@ from .sensor_config import sensor, pin, Adafruit_DHT
 def read_dht() -> tuple:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     return humidity, temperature
+
+def get_status() -> str:
+    humidity, temperature = read_dht()
+    return f"Humedad:{humidity:.1f}%\nTemperature: {temperature:.1f}"
