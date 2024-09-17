a26053192f904cb5e8f025b752b3c86095fe918c
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-17 10:26:51 -0600

    update read sensor function

--- raspberry/read.py
+++ raspberry/read.py
@@ -1,6 +1,6 @@
 from .sensor_config import sensor, pin, Adafruit_DHT
 
-def read_dht() -> tuple:
+async def read_dht() -> tuple:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     return humidity, temperature
 
--- src/status.py
+++ src/status.py
@@ -4,7 +4,7 @@ from raspberry.read import read_dht
 @bot.message_handler(commands=["status"])
 async def status(message):
     print("enviando status")
-    humidity, temperature = read_dht()
+    humidity, temperature = await read_dht()
     await bot.send_message(
         chat_id=message.chat.id, 
         text=f"Humedad:{humidity:.1f}%\nTemperature: {temperature:.1f}"
