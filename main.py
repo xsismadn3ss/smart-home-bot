368d5f44b81748461b38afe8451f3ba9716ccebb
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-17 10:01:34 -0600

    handle error done

--- main.py
+++ main.py
@@ -13,15 +13,8 @@ async def raspberry():
 
 
 async def main():
-    try:
-        rasp_task = asyncio.create_task(raspberry())
-        await bot.polling()
-        # asyncio.gather(rasp_task)
-    except KeyboardInterrupt:
-        print("Bot finalizado")
-    finally:
-        rasp_task.cancel()
-        await rasp_task
+    rasp_task = asyncio.create_task(raspberry())
+    await bot.polling()
 
 if __name__ == "__main__":
     print("Bot intialized 🤖")
