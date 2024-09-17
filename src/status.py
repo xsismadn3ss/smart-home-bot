96bcf1cc3a82cce3ad0d8924640c3d96a591db36
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-17 10:25:31 -0600

    edit status commands

--- src/status.py
+++ src/status.py
@@ -1,10 +1,11 @@
 from .bot_data import bot
-from raspberry.read import get_status
+from raspberry.read import read_dht
 
 @bot.message_handler(commands=["status"])
 async def status(message):
     print("enviando status")
+    humidity, temperature = read_dht()
     await bot.send_message(
         chat_id=message.chat.id, 
-        text=get_status()
+        text=f"Humedad:{humidity:.1f}%\nTemperature: {temperature:.1f}"
     )
