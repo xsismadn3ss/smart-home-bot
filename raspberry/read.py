592ebcaeb308fc344ca8b4cee5fd92ceff6cbcd0
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-17 10:28:24 -0600

    ...

--- raspberry/read.py
+++ raspberry/read.py
@@ -3,6 +3,7 @@ from .sensor_config import sensor, pin, Adafruit_DHT
 async def read_dht() -> tuple:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     print("enviando datos")
+    print(humidity, temperature)
     return humidity, temperature
 
 def get_status() -> str:
