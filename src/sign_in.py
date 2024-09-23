affe6bbc28f021d1f91d50feec85684df6589320
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-23 01:48:35 -0600

    update signin command

--- src/sign_in.py
+++ src/sign_in.py
@@ -4,5 +4,18 @@ from .bot_data import bot, temp
 async def sign_in(message):
     print("sign in...")
     chatid = message.chat.id
+    temp[chatid] = "login"
     print(chatid, type(chatid))
-    await bot.reply_to(message,"Ingrese su usuario")
+    await bot.reply_to(message,"Ingrese la contraseña del bot")
+
+
+@bot.message_handler(func=lambda message: temp.get(message.chat.id) == "login")
+async def sign_in_form(message):
+    chatid = message.chat.id
+    print(type(message))
+    test_password = "1234"
+    test = message.text
+    if test == test_password:
+        await bot.send_message(chatid, "Bienvenido")
+    else:
+        await bot.send_message(chatid, "Contraseña incorrecta")
