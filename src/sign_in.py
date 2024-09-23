e9225fa34c1f5133e7152e0595a8b3e3dd1000d4
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-23 01:33:20 -0600

    update signin command

--- src/sign_in.py
+++ src/sign_in.py
@@ -4,5 +4,5 @@ from .bot_data import bot, temp
 async def sign_in(message):
     print("sign in...")
     chatid = message.chat.id
-    print(chatid)
+    print(chatid, type(chatid))
     await bot.reply_to(message,"Ingrese su usuario")
