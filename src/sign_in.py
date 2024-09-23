c52618c93071c9f5d9c80f02bad1ee07df6aa44c
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-23 02:14:45 -0600

    update signin command

--- src/sign_in.py
+++ src/sign_in.py
@@ -2,6 +2,7 @@ from .bot_data import bot, temp
 import asyncio
 from fx.reset_temp import reset_temp
 
+
 @bot.message_handler(commands=["sign_in"])
 async def sign_in(message):
     print("sign in...")
@@ -10,12 +11,12 @@ async def sign_in(message):
     temp[chatid] = "login"
     temp["msgid"] = msgid
     print(chatid, type(chatid))
-    await bot.send_message(chatid,"Ingrese la contraseña del bot")
+    await bot.send_message(chatid, "Ingrese la contraseña del bot")
 
 
 @bot.message_handler(func=lambda message: temp.get(message.chat.id) == "login")
 async def sign_in_form(message):
-    
+
     chatid = message.chat.id
     print(type(message))
     test_password = "1234"
@@ -26,7 +27,7 @@ async def sign_in_form(message):
         await bot.send_message(chatid, "Bienvenido")
         await asyncio.sleep(0.2)
         await bot.delete_message(chat_id=chatid, message_id=temp["msgid"] + 1)
-        await bot.delete_message(chat_id=chatid, message_id=(temp["msgid"]  + 2))
+        await bot.delete_message(chat_id=chatid, message_id=(temp["msgid"] + 2))
     else:
         await bot.send_message(chatid, "Contraseña incorrecta")
         await asyncio.sleep(0.2)
