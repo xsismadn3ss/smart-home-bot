87e35e3216bcebbee5f37fdb04e38d35267a0751
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-25 01:58:15 -0600

    update status command

--- src/status.py
+++ src/status.py
@@ -1,7 +1,10 @@
 from .bot_data import bot
 from raspberry.read import get_status
+from src.fx.check_login import login_required
+
 
 @bot.message_handler(commands=["status"])
+@login_required
 async def status(message):
     await bot.send_message(
         chat_id=message.chat.id, 
