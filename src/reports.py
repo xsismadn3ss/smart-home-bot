f82fadd33fe77bc4d919f1fe86788e2aed68d9d2
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-25 10:39:05 -0600

    update reports command

--- src/reports.py
+++ src/reports.py
@@ -1,7 +1,14 @@
+from datetime import datetime
 from .bot_data import bot
+from data import humidity_queries, temperature_queries
 
 
 @bot.message_handler(commands=["reports"])
 async def reports(message):
-    print("Generando Grafica...")
-    await bot.reply_to(message,"soy una grafica.com")
+    today_h = humidity_queries.get_from_today()
+    dt = datetime.now()
+    path = "assets/{}_{}_{}_{}{}{}".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+    )
+    humidity_queries.get_chart()
+    await bot.reply_to(message, "Generando graficas")
