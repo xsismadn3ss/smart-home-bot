81e0ca9ea793b85fd13070b7f9f921bf9b5969eb
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-25 01:57:55 -0600

    creat login decorator

---
+++ src/fx/check_login.py
@@ -0,0 +1,15 @@
+from data import user_queries
+from src.bot_data import bot
+from functools import wraps
+
+def login_required(func):
+    @wraps(func)
+    async def wrapper(message, *args, **kwargs):
+        chatid = message.chat.id
+        user = user_queries.get(chat_id=chatid)
+
+        if user is None:
+            await bot.send_message(chatid, "Necesitas iniciar sesión...")
+        else:
+            return await func(message, *args, **kwargs)
+    return wrapper
