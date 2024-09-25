264a30bc24b6592be565f3cdc58524604d656c75
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-25 02:12:58 -0600

    añadiendo persistencia de datos

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import asyncio
 from src import *
 from src.bot_data import bot
 from raspberry.read import get_status
+from data import humidity_queries, temperature_queries
 
 async def raspberry():
     print("Leyendo sensores...")
+    i = 0
     while True:
         try:
-            data =await get_status()
-            print(data)
+            h, t = await get_status()
+            if i == 2:
+                print("guardando datos, reiniciando contador...")
+                humidity_queries.insert(h)
+                temperature_queries.insert(t)
+                i = 0
+
         except asyncio.CancelledError or KeyboardInterrupt:
             print("\nLecutura de sensores finalizada")
             break
 
+        asyncio.sleep(4.5)
+        i += 1
 
 async def main():
     rasp_task = asyncio.create_task(raspberry())
