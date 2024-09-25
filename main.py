ff371827ef099efb79096887b88a034f18b6b5b1
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-25 10:21:19 -0600

    update main

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from data import humidity_queries, temperature_queries
 async def raspberry():
     print("Leyendo sensores...")
     while True:
-        # try:
-        h, t = await get_status()
-        print(h,t)
-        print("guardando datos, reiniciando contador...")
-        humidity_queries.insert("Un error ha ocurrido: ", h)
-        temperature_queries.insert(t)
+        try:
+            h, t = await get_status()
+            print(h,t)
+            print("guardando datos, reiniciando contador...")
+            humidity_queries.insert(h)
+            temperature_queries.insert(t)
+            asyncio.sleep(4.5) 
 
-        # except Exception as e:
-        #     print(e)
-        #     print("\nLecutura de sensores finalizada o sensor no conectado")
-        #     break
+        except Exception as e:
+            print("Un error ha ocurrido: ",e)
+            print("\nLecutura de sensores finalizada o sensor no conectado")
+            break
 
-        asyncio.sleep(4.5)
 
 async def main():
     rasp_task = asyncio.create_task(raspberry())
