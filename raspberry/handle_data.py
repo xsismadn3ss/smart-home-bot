0b907a9e11a4df27b0ea1aad1268450545ba280e
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-28 01:11:07 -0600

    update handle data

--- raspberry/handle_data.py
+++ raspberry/handle_data.py
@@ -4,9 +4,17 @@ def avg(mesures):
     avg = sum(mesures)/ len(mesures)
     return avg
 
-async def save_data(h_list, t_list):
-    h_avg = avg(h_list)
-    t_avg = avg(t_list)
+async def save_data(h_list, t_list, count):
+    if count == 5:
+        print("Guardando datos...")
+        h_avg = avg(h_list)
+        t_avg = avg(t_list)
 
-    await humidity_queries.insert(h_avg)
-    await temperature_queries.insert(t_avg)
+        await humidity_queries.insert(h_avg)
+        await temperature_queries.insert(t_avg)
+        h_list = []
+        t_list = []
+        i = 0
+        return h_list, t_list, count
+    else:
+        return h_list, t_list, count
