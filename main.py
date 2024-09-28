9f8893ed24a3beaeff7230455fbf444d895adeab
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-28 02:02:45 -0600

    update main

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def raspberry():
             h, t = await read_dht()
             t_list.append(t); h_list.append(h)
             if i == 5:
-                h_list, t_list, i = await save_data(h_list, t_list)
+                h_list, t_list, i = await save_data(h_list, t_list, i)
         except Exception as e:
             print(e)
             break
