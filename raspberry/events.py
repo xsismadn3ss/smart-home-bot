52f54b674794be4283bbc34498c08aa2fed6889b
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-28 02:08:03 -0600

    update events

--- raspberry/events.py
+++ raspberry/events.py
@@ -48,8 +48,8 @@ async def report_state(state: bool) -> None:
 
 
 async def generateReports(time: datetime):
-
-    if time.hour > 20 or time.hour < 4 and load_config()["status"]["reports_sent"] == False:
+    config = await load_config()["status"]["reports_sent"]
+    if time.hour > 20 or time.hour < 4 and config == False:
         # load data
         h_data = await humidity_queries.get_from_today()
         t_data = await temperature_queries.get_from_today()
